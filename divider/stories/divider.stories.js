7c83d470aa7aa8d5b0521aca25ea892fe5fa121d
Remy <1789748+undefined@users.noreply.github.com>
2021-12-23 10:22:52 +0000

    Push from dddd

--- divider/stories/divider.stories.js
+++
@@ -1,7 +0,0 @@
-import { html } from '@microsoft/fast-element';
-import { provideDesignSystem, getDivider } from '@divriots/starter-furious';
-
-provideDesignSystem().register(getDivider());
-
-export const defaultStory = () => html` <fs-divider></fs-divider> `;
-defaultStory.storyName = 'Default';
